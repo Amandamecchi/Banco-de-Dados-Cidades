9543bf3a7937a7788701a8aaf9bf7e2c0332595c
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2024-10-01 15:38:19 -0300

    Create leonardo_pedro_sql.sql

---
+++ leonardo_pedro_sql.sql
@@ -0,0 +1,7 @@
+INSERT INTO cidades (estado, cidade, populacao, fundacao, prefeito) VALUES
+
+('São Paulo', 'São Paulo', 12396372, '1554-01-25', 'Ricardo Nunes'),
+('São Paulo', 'Campinas', 1213792, '1774-07-14', 'Dário Saadi'),
+('São Paulo', 'Santos', 433656, '1546-01-26', 'Rogério Santos'),
+('São Paulo', 'São José dos Campos', 737310, '1767-07-27', 'Anderson Farias'),
+('São Paulo', 'Ribeirão Preto', 711825, '1856-06-19', 'Duarte Nogueira'),
