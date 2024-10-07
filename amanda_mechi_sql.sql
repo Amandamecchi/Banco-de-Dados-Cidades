1ce1bd7a02cb61d74158c5c85921d8438b2ad8e7
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-10-07 17:57:32 -0300

    Add 80 UPDATES

---
+++ amanda_mechi_sql.sql
@@ -0,0 +1,93 @@
+/*20 UPDATES - Fundação*/
+
+UPDATE cidades_brasil SET fundacao = '1600-01-01' WHERE cidade = 'São Paulo';
+UPDATE cidades_brasil SET fundacao = '1780-05-10' WHERE cidade = 'Campinas';
+UPDATE cidades_brasil SET fundacao = '1550-02-15' WHERE cidade = 'Santos';
+UPDATE cidades_brasil SET fundacao = '1800-07-20' WHERE cidade = 'São José dos Campos';
+UPDATE cidades_brasil SET fundacao = '1700-06-25' WHERE cidade = 'Ribeirão Preto';
+UPDATE cidades_brasil SET fundacao = '1600-09-10' WHERE cidade = 'Sorocaba';
+UPDATE cidades_brasil SET fundacao = '1650-12-30' WHERE cidade = 'Guarulhos';
+UPDATE cidades_brasil SET fundacao = '1950-02-20' WHERE cidade = 'Osasco';
+UPDATE cidades_brasil SET fundacao = '1890-12-15' WHERE cidade = 'Belo Horizonte';
+UPDATE cidades_brasil SET fundacao = '1885-03-11' WHERE cidade = 'Uberlândia';
+UPDATE cidades_brasil SET fundacao = '1855-04-19' WHERE cidade = 'Juiz de Fora';
+UPDATE cidades_brasil SET fundacao = '1910-08-22' WHERE cidade = 'Contagem';
+UPDATE cidades_brasil SET fundacao = '1955-09-12' WHERE cidade = 'Betim';
+UPDATE cidades_brasil SET fundacao = '1830-07-03' WHERE cidade = 'Montes Claros';
+UPDATE cidades_brasil SET fundacao = '1750-02-17' WHERE cidade = 'Uberaba';
+UPDATE cidades_brasil SET fundacao = '1565-01-01' WHERE cidade = 'Rio de Janeiro';
+UPDATE cidades_brasil SET fundacao = '1580-11-15' WHERE cidade = 'Niterói';
+UPDATE cidades_brasil SET fundacao = '1840-05-31' WHERE cidade = 'Nova Iguaçu';
+UPDATE cidades_brasil SET fundacao = '1943-07-17' WHERE cidade = 'Duque de Caxias';
+UPDATE cidades_brasil SET fundacao = '1670-06-18' WHERE cidade = 'Campos dos Goytacazes';
+
+/*Estado*/
+
+UPDATE cidades_brasil SET estado = 'Rio de Janeiro' WHERE cidade = 'São Paulo';
+UPDATE cidades_brasil SET estado = 'Minas Gerais' WHERE cidade = 'Campinas';
+UPDATE cidades_brasil SET estado = 'Espírito Santo' WHERE cidade = 'Santos';
+UPDATE cidades_brasil SET estado = 'Paraná' WHERE cidade = 'São José dos Campos';
+UPDATE cidades_brasil SET estado = 'Santa Catarina' WHERE cidade = 'Ribeirão Preto';
+UPDATE cidades_brasil SET estado = 'Rio Grande do Sul' WHERE cidade = 'Sorocaba';
+UPDATE cidades_brasil SET estado = 'Minas Gerais' WHERE cidade = 'Guarulhos';
+UPDATE cidades_brasil SET estado = 'Rio de Janeiro' WHERE cidade = 'Osasco';
+UPDATE cidades_brasil SET estado = 'São Paulo' WHERE cidade = 'Belo Horizonte';
+UPDATE cidades_brasil SET estado = 'Paraná' WHERE cidade = 'Uberlândia';
+UPDATE cidades_brasil SET estado = 'Minas Gerais' WHERE cidade = 'Juiz de Fora';
+UPDATE cidades_brasil SET estado = 'Espírito Santo' WHERE cidade = 'Contagem';
+UPDATE cidades_brasil SET estado = 'Paraná' WHERE cidade = 'Betim';
+UPDATE cidades_brasil SET estado = 'Santa Catarina' WHERE cidade = 'Montes Claros';
+UPDATE cidades_brasil SET estado = 'Rio Grande do Sul' WHERE cidade = 'Uberaba';
+UPDATE cidades_brasil SET estado = 'Paraná' WHERE cidade = 'Rio de Janeiro';
+UPDATE cidades_brasil SET estado = 'Santa Catarina' WHERE cidade = 'Niterói';
+UPDATE cidades_brasil SET estado = 'Minas Gerais' WHERE cidade = 'Nova Iguaçu';
+UPDATE cidades_brasil SET estado = 'Paraná' WHERE cidade = 'Duque de Caxias';
+UPDATE cidades_brasil SET estado = 'Santa Catarina' WHERE cidade = 'Campos dos Goytacazes';
+
+/*População*/
+
+UPDATE cidades_brasil SET populacao = 13000000 WHERE cidade = 'São Paulo';
+UPDATE cidades_brasil SET populacao = 1220000 WHERE cidade = 'Campinas';
+UPDATE cidades_brasil SET populacao = 435000 WHERE cidade = 'Santos';
+UPDATE cidades_brasil SET populacao = 740000 WHERE cidade = 'São José dos Campos';
+UPDATE cidades_brasil SET populacao = 720000 WHERE cidade = 'Ribeirão Preto';
+UPDATE cidades_brasil SET populacao = 705000 WHERE cidade = 'Sorocaba';
+UPDATE cidades_brasil SET populacao = 1400000 WHERE cidade = 'Guarulhos';
+UPDATE cidades_brasil SET populacao = 700000 WHERE cidade = 'Osasco';
+UPDATE cidades_brasil SET populacao = 2400000 WHERE cidade = 'Belo Horizonte';
+UPDATE cidades_brasil SET populacao = 710000 WHERE cidade = 'Uberlândia';
+UPDATE cidades_brasil SET populacao = 580000 WHERE cidade = 'Juiz de Fora';
+UPDATE cidades_brasil SET populacao = 680000 WHERE cidade = 'Contagem';
+UPDATE cidades_brasil SET populacao = 440000 WHERE cidade = 'Betim';
+UPDATE cidades_brasil SET populacao = 410000 WHERE cidade = 'Montes Claros';
+UPDATE cidades_brasil SET populacao = 350000 WHERE cidade = 'Uberaba';
+UPDATE cidades_brasil SET populacao = 6800000 WHERE cidade = 'Rio de Janeiro';
+UPDATE cidades_brasil SET populacao = 520000 WHERE cidade = 'Niterói';
+UPDATE cidades_brasil SET populacao = 830000 WHERE cidade = 'Nova Iguaçu';
+UPDATE cidades_brasil SET populacao = 930000 WHERE cidade = 'Duque de Caxias';
+UPDATE cidades_brasil SET populacao = 510000 WHERE cidade = 'Campos dos Goytacazes';
+
+/*Prefeito*/
+
+UPDATE cidades_brasil SET prefeito = 'Bruno Souza' WHERE cidade = 'São Paulo';
+UPDATE cidades_brasil SET prefeito = 'Carlos Silva' WHERE cidade = 'Campinas';
+UPDATE cidades_brasil SET prefeito = 'João Mendes' WHERE cidade = 'Santos';
+UPDATE cidades_brasil SET prefeito = 'Paula Gomes' WHERE cidade = 'São José dos Campos';
+UPDATE cidades_brasil SET prefeito = 'Lucas Rocha' WHERE cidade = 'Ribeirão Preto';
+UPDATE cidades_brasil SET prefeito = 'Ana Freitas' WHERE cidade = 'Sorocaba';
+UPDATE cidades_brasil SET prefeito = 'Fernanda Lima' WHERE cidade = 'Guarulhos';
+UPDATE cidades_brasil SET prefeito = 'Gustavo Oliveira' WHERE cidade = 'Osasco';
+UPDATE cidades_brasil SET prefeito = 'Ricardo Leite' WHERE cidade = 'Belo Horizonte';
+UPDATE cidades_brasil SET prefeito = 'Marcos Almeida' WHERE cidade = 'Uberlândia';
+UPDATE cidades_brasil SET prefeito = 'Clara Moreira' WHERE cidade = 'Juiz de Fora';
+UPDATE cidades_brasil SET prefeito = 'Carlos Souza' WHERE cidade = 'Contagem';
+UPDATE cidades_brasil SET prefeito = 'Mariana Costa' WHERE cidade = 'Betim';
+UPDATE cidades_brasil SET prefeito = 'Gustavo Lira' WHERE cidade = 'Montes Claros';
+UPDATE cidades_brasil SET prefeito = 'Pedro Almeida' WHERE cidade = 'Uberaba';
+UPDATE cidades_brasil SET prefeito = 'Julia Fernandes' WHERE cidade = 'Rio de Janeiro';
+UPDATE cidades_brasil SET prefeito = 'Felipe Braga' WHERE cidade = 'Niterói';
+UPDATE cidades_brasil SET prefeito = 'Beatriz Santos' WHERE cidade = 'Nova Iguaçu';
+UPDATE cidades_brasil SET prefeito = 'Carlos Meireles' WHERE cidade = 'Duque de Caxias';
+UPDATE cidades_brasil SET prefeito = 'Henrique Mota' WHERE cidade = 'Campos dos Goytacazes';
+
+/*80 UPDATES*/
